315b8fb040d449c3d206165f8cc3c17ebac02c0c
Piyush Tomar <tomarpiyush918@gmail.com>
2026-09-22 20:46:00 +0530

    Update Home.jsx

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -44,7 +44,7 @@ export default function Home() {
           </div>
           <div className="dash-greeting-actions">
             <button className="btn ghost inline" onClick={() => go("plan")}>{t("tile_plan")}</button>
-            <button className="btn primary inline" onClick={() => go("chat")}>+ New conversation</button>
+            <button className="btn primary inline" onClick={() => go("chat")}>+ New Query</button>
           </div>
         </div>
 
